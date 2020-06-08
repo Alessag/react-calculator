c82258b505a6b7dd7b034f09f0ebf4e1724ea4f4
Alessandra <alessamica@gmail.com>
2020-06-07 22:39:35 -0400

    Calculator component

---
+++ src/Components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/styles.css';
+
+function Calculator() {
+  return (
+    <div className='calculator'>
+      <h1>I am a calculator</h1>
+    </div>
+  );
+}
+
+export default Calculator;
