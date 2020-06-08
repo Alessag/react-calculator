bf7f8309af3ca448f0c949b0ed9687f0ba71b6d3
Alessandra <alessamica@gmail.com>
2020-06-08 11:56:35 -0400

    Calculator default screen values, props and set a state

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/styles.css';
 import Screen from './Screen';
 import Keypad from './Keypad';
 
-function Calculator() {
-  return (
-    <div className='calculator'>
-      <Screen />
-      <Keypad />
-    </div>
-  );
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      equation: 'Hola',
+      result: 0,
+    };
+  }
+
+  render() {
+    const { equation, result } = this.state;
+    return (
+      <div className='calculator'>
+        <Screen equation={equation} result={result} />
+        <Keypad />
+      </div>
+    );
+  }
 }
 
 export default Calculator;
