cfb9896306510ab984a3b557ff8fd17b70797d1d
Alessandra <alessamica@gmail.com>
2020-06-07 23:01:50 -0400

    Import Screen component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/styles.css';
+import Screen from './Screen';
 
 function Calculator() {
   return (
     <div className='calculator'>
-      <h1>I am a calculator</h1>
+      <Screen />
     </div>
   );
 }
