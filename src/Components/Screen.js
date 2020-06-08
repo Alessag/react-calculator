94af2e8adad041b955a575a7d37abddf9daf218e
Alessandra <alessamica@gmail.com>
2020-06-08 11:13:54 -0400

    Checking prop types in Screen component and destructuring the props

--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import OperationScreen from './OperationScreen';
 import ResultScreen from './ResultScreen';
 import '../styles/styles.css';
 
-function Screen() {
+function Screen(props) {
+  const { equation } = props;
   return (
     <div className='screen'>
-      <OperationScreen />
+      <OperationScreen>{equation}</OperationScreen>
       <ResultScreen />
     </div>
   );
 }
+
+Screen.propTypes = {
+  equation: PropTypes.number.isRequired,
+};
 
 export default Screen;
